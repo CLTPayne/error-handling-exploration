532cf19e9e1e7765c3f2007decce0354c203a5b7
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-05-18 18:05:39 +0100

    Create a custom error with specified properties added.

---
+++ customError.js
@@ -0,0 +1,33 @@
+class APIResponseError extends Error {
+    constructor(status, url = 'unknown', ...params) {
+        super(...params);
+
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, APIResponseError);
+        }
+
+        this.name = 'API Response Error';
+        this.status = status;
+        this.url = url;
+    }
+}
+
+try {
+    throw new APIResponseError(
+        500, 
+        'https://randomfakeapi.com/endpoint',
+        'Server Error: Could not connect'
+    );
+} catch (error) {
+    console.log(error.name); // API Response Error
+    console.log(error.status); // 500
+    console.log(error.url); // 'https://randomfakeapi.com/endpoint'
+    console.log(error.message); // Server Error: Could not connect
+}
+
+// Extend the Error class to make a custom error with any properties 
+// E.g. a url and response status could be really helpful for api calls 
+// And these do not exist on default JavaScript error types
+// The name property would be a generic 'Error' but this is overridden 
+// Hence the more useful 'API Response Error'.
+
