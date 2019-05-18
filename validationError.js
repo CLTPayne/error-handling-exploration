5b207c8e6fe2987e0bf2d2532b2b544380616549
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-05-18 18:40:15 +0100

    Example implementation of validation error

--- validationError.js
+++ validationError.js
@@ -5,8 +5,9 @@ class ValidationError extends Error {
     }
 }
 
+// Dummy function to generate an error
 const test = () => {
-    throw new ValidationError('The user input is not correct');
+    throw new ValidationError("The user input is not correct");
 }
 
 try {
@@ -15,4 +16,51 @@ try {
     console.log(error.message);
     console.log(error.name);
     console.log(error.stack);
+}
+
+// We set the message property on the parent error via super()
+// We over ride the parent constructors name of 'Error' with 'Validation Error'
+
+// Usage for this custom error:
+
+const readUser = (json) => {
+    const user = JSON.parse(json);
+
+    if (!user.age) {
+        throw new ValidationError("No age field");
+    }
+    if (!user.name) {
+        throw new ValidationError("No name field");
+    }
+
+    return user;
+}
+
+// Example of this in practise
+// Generate a validation error with missing name property:
+
+try {
+    const user = readUser('{"name": 28 }');
+} catch (error) {
+    if (error instanceof ValidationError) {
+        console.log(`Invalid data: ${error.message}`);
+    } else if (error instanceof SyntaxError) {
+        console.log(`JSON Syntax Error: ${error.message}`);
+    } else {
+        throw error // if it's an unknown error, throw it
+    }
+}
+
+// Generate a syntax error with unexpected number in JSON position 1:
+
+try {
+    const user = readUser('{1: 28 }');
+} catch (error) {
+    if (error instanceof ValidationError) {
+        console.log(`Invalid data: ${error.message}`);
+    } else if (error instanceof SyntaxError) {
+        console.log(`JSON Syntax Error: ${error.message}`);
+    } else {
+        throw error // if it's an unknown error, throw it
+    }
 }
